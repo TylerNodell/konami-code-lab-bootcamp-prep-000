720eb6a3ae04a6e4859c664a04d541f0b1df737f
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-26 20:50:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  var checkCode = [];
+  window.addEventListener('keydown', (event) => {
+    checkCode.push(event.key)
+  })
+  if (checkCode.length === 10) {
+    if (checkCode === codes) {
+      alert('You got it!')
+    } else {
+      alert('Wrong!')
+      checkCode = []
+    }
+  }
 }
+
+init()
