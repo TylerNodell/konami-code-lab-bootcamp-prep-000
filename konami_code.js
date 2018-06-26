b8cc0fc77e84a5db6e504ca22cdcad13b5f2ad2f
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-26 21:43:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -18,20 +18,14 @@ function init() {
   input.addEventListener('keydown', (event) => {
     checkCode.push(event.key)
   if (checkCode.length === 10) {
-    // var counter = 0;
-    // for (var i = 0; i < 10; i++) {
-    //   if (checkCode[i] === codes[i]) {
-    //   counter++
-    //     }
-    //   }
-    // if (counter === 10) {
-    //     alert('yes')
-    //     checkCode = []
-    // } else {
-    //   checkCode = []
-    // }
-    if (checkCode === code) {
-      alert('Success')
+    var counter = 0;
+    for (var i = 0; i < 10; i++) {
+      if (checkCode[i] === codes[i]) {
+      counter++
+        }
+      }
+    if (counter === 10) {
+      alert('yes')
       checkCode = []
     } else {
       checkCode = []
